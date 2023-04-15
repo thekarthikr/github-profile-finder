912bbc21b1225c919895d297e35aa797e7c909b0
Karthik <karthikwebdev@outlook.com>
2023-04-15 14:58:42 +0530

    Add Loader

---
+++ src/components/layouts/Spinner.jsx
@@ -0,0 +1,14 @@
+import "./Spinner.css";
+
+function Spinner() {
+  return (
+    <div className='spinner mx-auto '>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Spinner;
