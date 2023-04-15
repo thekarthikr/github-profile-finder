90cb64cb3fb76acc460a4c712e3576e6505c51bc
Karthik <karthikwebdev@outlook.com>
2023-04-15 17:58:45 +0530

    Update Loader

--- src/components/context/github/GithubContext.jsx
+++ src/components/context/github/GithubContext.jsx
@@ -1,25 +1,36 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext();
 
 const GITHUB_URL = import.meta.env.VITE_GITHUB_URL;
 
 export const GithubProvider = ({ children }) => {
-  const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const initialState = {
+    users: [],
+    isLoading: false,
+  };
+
+  const [state, dispatch] = useReducer(githubReducer, initialState);
 
   const fetchUsers = async () => {
+    setLoading();
+
     const res = await fetch(`${GITHUB_URL}/users`);
     const data = await res.json();
-    setUsers(data);
-    setIsLoading(false);
+    dispatch({
+      type: "GET_USERS",
+      payload: data,
+    });
   };
+
+  const setLoading = () => dispatch({ type: "SET_LOADING" });
 
   return (
     <GithubContext.Provider
       value={{
-        users,
-        isLoading,
+        users: state.users,
+        isLoading: state.isLoading,
         fetchUsers,
       }}
     >
