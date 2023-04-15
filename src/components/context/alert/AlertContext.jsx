61ee4d1fde052790a365cd42adeee1c451acad98
Karthik <karthikwebdev@outlook.com>
2023-04-15 18:50:20 +0530

    Add Alert Context and Reducer

---
+++ src/components/context/alert/AlertContext.jsx
@@ -0,0 +1,31 @@
+import { createContext, useReducer } from "react";
+import alertReducer from "./AlertReducer";
+
+const AlertContext = createContext();
+
+export const AlertProvider = ({ children }) => {
+  const initialState = null;
+
+  const [state, dispatch] = useReducer(alertReducer, initialState);
+
+  const setAlert = (message, text) => {
+    dispatch({
+      type: "SET_ALERT",
+      payload: { message, text },
+    });
+
+    setTimeout(() => {
+      dispatch({
+        type: "REMOVE_ALERT",
+      });
+    }, 3000);
+  };
+
+  return (
+    <AlertContext.Provider value={{ alert: state }}>
+      {children}
+    </AlertContext.Provider>
+  );
+};
+
+export default AlertContext;
