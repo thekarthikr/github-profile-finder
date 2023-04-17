634b390374b363b38f1407f9b8c3faaf9de2f05b
Karthik <karthikwebdev@outlook.com>
2023-04-17 14:12:49 +0530

    add user details

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -44,7 +44,7 @@ function User() {
           </Link>
         </div>
 
-        <div className='grid grid-cols-1 md:grid-cols-3 mb-8 md:gap-8'>
+        <div className='grid grid-cols-1 md:grid-cols-32 lg:grid-cols-2 mb-8 md:gap-8'>
           <div className='avatar  mb-6 md:mb-0'>
             <div className='rounded  '>
               <figure>
@@ -76,6 +76,74 @@ function User() {
                   Visit Github Profile{" "}
                 </a>
               </div>
+            </div>
+
+            <div className='w-full rounded lg shadow-md bg-base-00 stats'>
+              {location && (
+                <div className='stat'>
+                  <div className='stat-title text-md'>
+                    Location
+                    <div className='text-lg stat-value'>{location}</div>
+                  </div>
+                </div>
+              )}
+
+              {blog && (
+                <div className='stat'>
+                  <div className='stat-title text-md'>
+                    Website
+                    <div className='text-lg stat-value'>{blog}</div>
+                  </div>
+                </div>
+              )}
+
+              {twitter_username && (
+                <div className='stat'>
+                  <div className='stat-title text-md'>
+                    Twitter
+                    <div className='text-lg stat-value'>{twitter_username}</div>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+
+        <div className='w-full py-5 mb-6 rounded-lg shadow-md bg-base stats'>
+          <div className='stat'>
+            <div className='stat-figure text-secondary'>
+              <FaUsers className='text-3xl md:text-5xl' />
+            </div>
+            <div className='stat-title pr-5'>Followers</div>
+            <div className='stat-value pr-5 text-3xl md:text-4xl'>
+              {followers}
+            </div>
+          </div>
+          <div className='stat'>
+            <div className='stat-figure text-secondary'>
+              <FaUserFriends className='text-3xl md:text-5xl' />
+            </div>
+            <div className='stat-title pr-5'>Following</div>
+            <div className='stat-value pr-5 text-3xl md:text-4xl'>
+              {following}
+            </div>
+          </div>
+          <div className='stat'>
+            <div className='stat-figure text-secondary'>
+              <FaCodepen className='text-3xl md:text-5xl' />
+            </div>
+            <div className='stat-title pr-5'>Public Repos</div>
+            <div className='stat-value pr-5 text-3xl md:text-4xl'>
+              {public_repos}
+            </div>
+          </div>
+          <div className='stat'>
+            <div className='stat-figure text-secondary'>
+              <FaStore className='text-3xl md:text-5xl' />
+            </div>
+            <div className='stat-title pr-5'>Public Gists</div>
+            <div className='stat-value pr-5 text-3xl md:text-4xl'>
+              {public_gists}
             </div>
           </div>
         </div>
