0d2be6a20422c87fbccc0fc32c7c2ab253fabebb
Karthik <karthikwebdev@outlook.com>
2023-04-17 16:40:27 +0530

    Update About section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,19 @@
 function About() {
-  return <div>About</div>;
+  return (
+    <div>
+      <h2 className='text-3xl mb-8'>Github Profile Finder</h2>
+      <p className='text-lg w-3/4'>
+        Simple Github Profile Finder is a tool that allows you to search for
+        Github profiles quickly and easily. With our platform, you can view
+        social links, bio information, and repositories associated with a
+        particular Github user. Our user-friendly interface makes it easy to
+        find the information you need, whether you are a developer or recruiter
+        searching for potential candidates or just curious about someone's
+        Github activity. Discover valuable insights and explore talented
+        developers with Simple Github Profile Finder.
+      </p>
+    </div>
+  );
 }
 
 export default About;
