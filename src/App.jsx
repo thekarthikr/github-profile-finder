ced6a129dcefff48cabdd314692b2b48d8ee2da8
Karthik <karthikwebdev@outlook.com>
2023-04-17 16:56:55 +0530

    Update router

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
                 <Route path='/' element={<Home />} />
                 <Route path='/about' element={<About />} />
                 <Route path='/user/:login' element={<User />} />
-                <Route path='/notfound' element={<NotFound />} />
+                <Route path='/*' element={<NotFound />} />
               </Routes>
             </main>
             <Footer />
